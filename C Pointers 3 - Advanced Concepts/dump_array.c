61fa8561b85a69a07b598ed8ea87d3ac1d5c7377
Simone Massaro <49060178+simone7687@users.noreply.github.com>
2020-12-16 11:41:32 +0100

    Initial commit

---
+++ C Pointers 3 - Advanced Concepts/dump_array.c
@@ -0,0 +1,60 @@
+/* vim: set tabstop=4 expandtab shiftwidth=4 softtabstop=4: */
+
+#include <assert.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+static void dump_array(const void *base, size_t num_elem, size_t elem_size, void (*dump_element)(const void *, FILE *), FILE *fp);
+static void dump_int(const void *p, FILE *fp);
+static void dump_string(const void *p, FILE *fp);
+
+
+void dump_array(const void *base, size_t num_elem, size_t elem_size, void (*dump_element)(const void *, FILE *), FILE *fp)
+{
+    assert( base != NULL );
+    assert( fp != NULL );
+
+    /* TO STUDENTS:
+     *  Remove the following two lines and put here your implementation. */
+    fprintf(stderr, "To be implemented!\n");
+    abort();
+}
+
+void dump_int(const void *p, FILE *fp)
+{
+    assert( p != NULL );
+    assert( fp != NULL );
+
+    /* TO STUDENTS:
+     *  Remove the following two lines and put here your implementation. */
+    fprintf(stderr, "To be implemented!\n");
+    abort();
+}
+
+void dump_string(const void *p, FILE *fp)
+{
+    assert( p != NULL );
+    assert( fp != NULL );
+
+    /* TO STUDENTS:
+     *  Remove the following two lines and put here your implementation. */
+    fprintf(stderr, "To be implemented!\n");
+    abort();
+}
+
+int main()
+{
+    int iary[] = {1, 20, 25, 32, 76, 123};
+    const char *sary[] = {"e01","e02","e03","e04","e05","e06"};
+    FILE *fp = stdout;
+
+    dump_array(iary, sizeof iary/sizeof iary[0], sizeof iary[0], dump_int, fp);
+    fputc('\n', fp);
+    dump_array(sary, sizeof sary/sizeof sary[0], sizeof sary[0], dump_string, fp);
+    fputc('\n', fp);
+
+    return 0;
+}
