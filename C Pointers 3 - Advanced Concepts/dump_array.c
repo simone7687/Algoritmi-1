11a38c3f999e3144159f4e0fb59e1aa28ffcd50d
Simone Massaro <49060178+simone7687@users.noreply.github.com>
2020-12-31 11:02:04 +0100

    Update dump_array.c

--- C Pointers 3 - Advanced Concepts/dump_array.c
+++ C Pointers 3 - Advanced Concepts/dump_array.c
@@ -11,16 +11,31 @@ static void dump_array(const void *base, size_t num_elem, size_t elem_size, void
 static void dump_int(const void *p, FILE *fp);
 static void dump_string(const void *p, FILE *fp);
 
-
+/**
+ * https://youtu.be/g4PMtXow5Lo?list=PL6EeG-tt2Es75K50cuoPYjXdNbJR4yduu&t=3917
+ * stampo il contenuto di un array
+ * *base, num_elem, elem_size: come prima
+ * elem_size : sizeof(int)
+ * dump_elementi: una funzione che stampa un singolo elemento dell'array cioè const void*, e lo stampa in File
+ */
 void dump_array(const void *base, size_t num_elem, size_t elem_size, void (*dump_element)(const void *, FILE *), FILE *fp)
 {
     assert( base != NULL );
     assert( fp != NULL );
 
-    /* TO STUDENTS:
-     *  Remove the following two lines and put here your implementation. */
-    fprintf(stderr, "To be implemented!\n");
-    abort();
+    // Implementazione
+    // TODO: unsigned???? lo usato perchè lo fa anche il prof ma non so cosa sia
+    unsigned char *base_char = (unsigned char *)base;
+
+    while(num_elem) // fin quando n è diverso da 0
+    {
+        // ..............
+        (*dump_element)((void *)base_char, fp);
+        // .................
+        base_char = base_char+elem_size;
+
+        --num_elem;
+    }
 }
 
 void dump_int(const void *p, FILE *fp)
@@ -28,10 +43,9 @@ void dump_int(const void *p, FILE *fp)
     assert( p != NULL );
     assert( fp != NULL );
 
-    /* TO STUDENTS:
-     *  Remove the following two lines and put here your implementation. */
-    fprintf(stderr, "To be implemented!\n");
-    abort();
+    // Implementazione
+    // ......
+    printf("%d ", *(int *)(p));
 }
 
 void dump_string(const void *p, FILE *fp)
@@ -39,10 +53,13 @@ void dump_string(const void *p, FILE *fp)
     assert( p != NULL );
     assert( fp != NULL );
 
-    /* TO STUDENTS:
-     *  Remove the following two lines and put here your implementation. */
-    fprintf(stderr, "To be implemented!\n");
-    abort();
+    // Implementazione
+    // .......
+    unsigned char *p_char = (unsigned char *)p;
+    // .............
+    // sizeof(char) non va bene
+    for (size_t k = 0; k < sizeof(char); ++k)
+        printf("%c -", (char) *(p_char+k));
 }
 
 int main()
