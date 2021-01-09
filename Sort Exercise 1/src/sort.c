270d7b98e086f0c6e34c5c5aae8b42c91c47be5e
Simone Massaro <49060178+simone7687@users.noreply.github.com>
2021-01-09 17:12:01 +0100

    Sort ex1

--- Sort Exercise 1/src/sort.c
+++ Sort Exercise 1/src/sort.c
@@ -25,6 +25,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+// per lo swap
+#include <upo/utility.h>
 
 /**
  * https://youtu.be/Qj48bXW7zd4?list=PL6EeG-tt2Es75K50cuoPYjXdNbJR4yduu&t=2483
@@ -46,6 +48,7 @@
 void upo_insertion_sort(void *base, size_t n, size_t size, upo_sort_comparator_t cmp)
 {
     // Implementazione
+    // TODO: ancora da implementare
 }
 
 /**
@@ -60,6 +63,8 @@ void upo_insertion_sort(void *base, size_t n, size_t size, upo_sort_comparator_t
  * 3-   Ognuna di queste sottosequenze viene ordinata, applicando ricorsivamente l'algoritmo (impera)
  * 4-   Le due sottosequenze ordinate vengono fuse (combina). Per fare questo, si estrae ripetutamente il minimo delle due sottosequenze e lo si pone nella sequenza in uscita, che risulterà ordinata
  */
+void upo_merge(void *base, size_t lo, size_t mid, size_t hi, size_t size, upo_sort_comparator_t cmp);
+void upo_merge_sort_driver_topdown(void *base, size_t lo, size_t hi, size_t size, upo_sort_comparator_t cmp);
 void upo_merge_sort(void *base, size_t n, size_t size, upo_sort_comparator_t cmp)
 {
     // Implementazione
@@ -79,6 +84,7 @@ void upo_merge_sort_driver_topdown(void *base, size_t lo, size_t hi, size_t size
 {
     size_t mid;
     // se non si sono incrociati
+    // serve a bloccare il ciclo di "Ricorsione"(Ricorsiva)
     if (lo >= hi)
     {
         return;
@@ -91,16 +97,18 @@ void upo_merge_sort_driver_topdown(void *base, size_t lo, size_t hi, size_t size
     // https://www.geeksforgeeks.org/merge-sort/
     // Ordina la metà sinistra (Ricorsiva)
     upo_merge_sort_driver_topdown(base, lo, mid, size, cmp);
-    // Ordina la metà giusta (Ricorsiva)
+    // Ordina la metà destra (Ricorsiva)
     upo_merge_sort_driver_topdown(base, mid+1, hi, size, cmp);
 
-    // Unisce i risultati
+    // fa il merge 
     upo_merge(base, lo, mid, hi, size, cmp);
 }
 
 /**
  * https://youtu.be/aM6ZzJJ0u3o?list=PL6EeG-tt2Es75K50cuoPYjXdNbJR4yduu&t=1102
- * viene fatto in modo ricorsivo
+ * è l'estensione di upo_merge_sort_driver_topdown
+ * non è ricorsiva di perse ma viene fatta in modo ricorsivo perche upo_merge_sort_driver_topdown è ricorsiva
+ * 
  * e per parametroo riceve sempre una stringa divisa in due metà, oglio meta è ordinata
  * esempio:
  * base[]: 1 4 7   2 6 8
@@ -108,7 +116,7 @@ void upo_merge_sort_driver_topdown(void *base, size_t lo, size_t hi, size_t size
 void upo_merge(void *base, size_t lo, size_t mid, size_t hi, size_t size, upo_sort_comparator_t cmp)
 {
     // è l'array mase, noi lo utilizeremo solo da ptr[lo] a ptr[hi]
-    // inseriremo i "risultati" qui denzo (cio dentro a base)
+    // inseriremo i "risultati" qui dentro (cioè dentro a base)
     unsigned char *ptr = base;
     // l'array base da lo a hi
     // verra preso un consideracome per modificare l'arrray base
@@ -173,7 +181,86 @@ void upo_merge(void *base, size_t lo, size_t mid, size_t hi, size_t size, upo_so
     free(aux);
 }
 
+
+//TODO: devo ancora testarla
+int upo_quick_sort_partion(void *base, size_t lo, size_t hi, size_t size, upo_sort_comparator_t cmp);
+void upo_quick_sort_driver_topdown(void *base, size_t lo, size_t hi, size_t size, upo_sort_comparator_t cmp);
 void upo_quick_sort(void *base, size_t n, size_t size, upo_sort_comparator_t cmp)
 {
     // Implementazione
+    assert( base != NULL );
+    // quick sort su l'intero array
+    upo_quick_sort_driver_topdown(base, 0, n-1, size, cmp);
+}
+
+void upo_quick_sort_driver_topdown(void *base, size_t lo, size_t hi, size_t size, upo_sort_comparator_t cmp)
+{
+    int pi;
+    // 
+    if (lo >= hi)
+    {
+        return;
+    }
+
+    // 
+    pi = upo_quick_sort_partion(base, lo, hi, size, cmp);
+
+    // 
+    upo_quick_sort_driver_topdown(base, lo, pi-1, size, cmp);
+    // 
+    upo_quick_sort_driver_topdown(base, pi+1, hi, size, cmp);
+}
+
+int upo_quick_sort_partion(void *base, size_t lo, size_t hi, size_t size, upo_sort_comparator_t cmp)
+{
+    // è l'array base, noi lo utilizeremo solo da ptr[lo] a ptr[hi]
+    // inseriremo i "risultati" qui dentro (cioè dentro a base)
+    unsigned char *ptr = base;
+    // l'array base da lo a hi
+    // verra preso un consideracome per modificare l'arrray base
+    unsigned char *aux = NULL;
+
+    // aux[n]:  è ultimo elemento di aux[]
+    size_t n = hi-lo+1;
+
+    // aux[i]: iniziera da zero(ptr[lo]) fino arrivare a j
+    size_t i = lo;
+    //
+    size_t p = hi;
+
+    //
+    size_t k;
+    aux = malloc(n*size);
+    if (aux == NULL)
+    {
+        perror("Unable to allocate memory for auxiliary vector");
+        abort();
+    }
+    // Copia base [lo], ..., base [hi] nell'array ausiliario
+    // aux: destinazione
+    // ptr+lo*size: sposta la posizione di ptr su lo
+        // ptr: l'array di sorgete
+        // lo*size: dimensione totale dei elementi da 0 a lo
+    // n*size: dimensione dei elementi da lo a hi
+    memcpy(aux, ptr+lo*size, n*size);
+
+
+    // adesso fa il merge, cioè
+    // condidera aux in due metà(ordinate) cioè aux[i]-aux[j] e aux[j]-aux[n]
+    // i verra usato per la prima meta, j per la seconda
+
+    // considera aux[i] e aux[j], il più piccolo valore viene inserito in ptr[k], poi fa k++ e 
+        // aux[i] è il più piccolo i++
+        // altrimenti j++
+    for (k = lo; k <= hi; ++k)
+    {
+        if (cmp(ptr+k*size, ptr+p*size) < 0)
+        {
+            upo_swap(ptr+i*size, ptr+k*size, size);
+            i++;
+        }
+    }
+    upo_swap(ptr+i*size, ptr+hi*size, size);
+    // IMPORTANTE: da scrivere
+    return i;
 }
